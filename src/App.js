1a34bff0b67f9d8a21e5e64a0a16eb6fa8b2e016
Ryan Ho <rhohoman@gmail.com>
2019-08-05 08:50:50 -0400

    added a few validations and cleaned up display page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import { JSONDATA } from './data.js';
 import DataDisplayTable from './DataDisplayTable';
 import DataDisplay from './DataDisplay';
-import { Button, Form, Header } from 'semantic-ui-react';
+import { Button, Input, TextArea } from 'semantic-ui-react';
 
 
 class App extends React.Component{
@@ -26,7 +26,6 @@ class App extends React.Component{
 
 
   getKeys = () => {
-        //here I manipulate the data and get just the keys so I can use them for
         let dataArray = [...this.state.data]
         let keys = dataArray.map(object => Object.keys(object))[0]
 
@@ -42,16 +41,14 @@ class App extends React.Component{
   }
 
   backToDisplayTable = () => {
-    console.log(`back!`)
 
     this.setState({
       displayTable: true,
-    }, () => console.log(this.state.displayTable))
+    })
     
   }
 
   handleIdSearchChange = (event) => {
-    console.log(event.target.value)
     this.setState({
       idSearchValue: event.target.value
     })
@@ -59,15 +56,14 @@ class App extends React.Component{
 
   handleIdSearchIdSubmit = (event) => {
     event.preventDefault()
-    // console.log('event: ', event.target.querySelector('input').value)
     let id = this.state.idSearchValue
-    //search throguh all data to find object with this id and then send it to the data Display
     let data = [...this.state.data]
-
-    // data.filter(dataObject => dataObject.includes(id))
     let searchObject = data.filter(dataObject => dataObject.id == id)[0]
 
-    // console.log(searchObject)
+    if (searchObject == null){
+      alert('id does not exist')
+      return
+    }
 
     this.setState((prevState) => ({
         displayTable: !prevState,
@@ -77,7 +73,6 @@ class App extends React.Component{
   }
 
   handleFilterChange = (event) => {
-    // console.log(`${event.target.name}: `,event.target.value)
     let filterKey = event.target.name
     let filterTerm = event.target.value
     this.setState({
@@ -88,32 +83,33 @@ class App extends React.Component{
   handleDataTableFilter = (filterKey,filterTerm) => {
     let data = this.state.data
     let filteredData = []
-    // console.log(`filterKey: `,filterKey)
-    // console.log(`filterTerm: `,filterTerm)
 
     if( (filterKey == 'id') || (filterKey == 'impressions') || (filterKey == 'clicks')){
       filteredData = data.filter(obj => obj[filterKey].toString().includes(filterTerm))
+      if(filteredData.length == 0){
+        alert('no data matches this filter term')
+      }
     } else {
-      //ignoring null values
       filteredData = data.filter(obj => obj[filterKey] !== null)
       filteredData = filteredData.filter(obj => obj[filterKey].includes(filterTerm))
+      if(filteredData.length == 0){
+        alert('no data matches this filter term')
+      }
     }
 
     this.setState({
       filteredTableData: filteredData
-    },() => console.log(`filtered Table Data: `,this.state.filteredTableData))
+    })
 
   }
 
   filterFormDisplay = (event) => {
-    console.log(event.target.innerText)
     this.setState({
       filterForm: event.target.innerText
     })
   }
 
   resetTableData = (event) => {
-    //clear filters
     event.preventDefault()
     document.getElementById('filterForm').reset()
     this.setState({
@@ -123,58 +119,53 @@ class App extends React.Component{
   }
 
   sortTableData = (sortType) => {
-    //sort by impressions or clicks
+
     let data = [...this.state.data]
     let sortedData = []
-    console.log('sortType: ', sortType)
-    console.log(this.state.sorted)
 
     if (sortType == 'impressions'){
-      console.log('impressions')
-      // this.state.sorted ? sortedData = data.map(obj => obj[sortType]).sort(function(a, b){return b-a}) : sortedData = data.map(obj => obj[sortType]).sort(function(a, b){return a-b})
-      sortedData = this.state.sorted ? data.sort(function(a,b) {return b.impressions - a.impressions}) : data.sort(function(a,b) {return a.impressions - b.impressions})
-      // debugger
-      // this.setState((prevState) => ({
-      //   sorted: !prevState,
-      //   filteredTableData: sortedData,
-      // },() => console.log('did it hit?')))
+
+      sortedData = this.state.sorted ? data.sort(function(a,b) {return a.impressions - b.impressions}) : data.sort(function(a,b) {return b.impressions - a.impressions})
+
       this.setState({
         sorted: !this.state.sorted,
         filteredTableData: sortedData,
-      },() => console.log('did it hit?'))
+      })
     } else if (sortType == 'clicks'){
+      sortedData = this.state.sorted ? data.sort(function(a,b) {return a.impressions - b.impressions}) : data.sort(function(a,b) {return b.impressions - a.impressions})
 
-      console.log('clicks')
-
-      sortedData = this.state.sorted ? data.sort(function(a,b) {return b.impressions - a.impressions}) : data.sort(function(a,b) {return a.impressions - b.impressions})
       this.setState({
         sorted: !this.state.sorted,
         filteredTableData: sortedData,
-      },() => console.log('did it hit?'))
+      })
     }
-    //sort data displayed with the sort Type
-
   }
 
 
   render(){
     const properties = this.getKeys()
     const filteringType = this.state.filterForm
-    const formDisplay = filteringType == null ? null : <form id='filterForm'> {filteringType.charAt(0).toUpperCase() + filteringType.slice(1)} <input onChange={(event) => this.handleFilterChange(event)} name={filteringType} placeholder='filtering...'/> <button onClick={(event) => this.resetTableData(event)}>clear filtering</button></form>
+    const formDisplay = filteringType == null ? null :
+      <form id='filterForm'> {filteringType.charAt(0).toUpperCase() + filteringType.slice(1)}
+        <input onChange={(event) => this.handleFilterChange(event)} name={filteringType} placeholder='filtering...'/>
+        <button onClick={(event) => this.resetTableData(event)}>clear filtering</button>
+      </form>
+
     return (
       <div>
         {this.state.displayTable === true ? 
           <div>
             <br/>
+            <br/>
+            <br/>
             <form onSubmit={(event) => this.handleIdSearchIdSubmit(event)}>
-              Search: <input placeholder='search by id' name='idSearchValue' id='idSearchValue' onChange={(event) => this.handleIdSearchChange(event)}/><button >submit</button>
+              {/* Search: <input placeholder='search by id' name='idSearchValue' id='idSearchValue' onChange={(event) => this.handleIdSearchChange(event)}/><button >submit</button> */}
+              Search by Id <Input action={{ icon: 'search' }} placeholder='id here' name='idSearchValue' id='idSearchValue' onChange={(event) => this.handleIdSearchChange(event)}/>
             </form>
             <br/>
             <br/>
-            <br/>
-            <br/>
 
-            <h2>Filtering Options</h2>
+            <h3>Filtering Options</h3>
             <Button.Group>
               <Button onClick={(event) => this.filterFormDisplay(event)}>id</Button>
               <Button onClick={(event) => this.filterFormDisplay(event)}>pdf</Button>
@@ -190,7 +181,7 @@ class App extends React.Component{
             <DataDisplayTable data={this.state.filteredTableData.length == 0 ? this.state.data : this.state.filteredTableData} properties={properties} displayCallback={this.displayCallback} sortTableData={this.sortTableData}/>
           </div>
             :
-          <DataDisplay displayObject={this.state.displayObject} backToDisplayTable={this.backToDisplayTable}/>
+          <DataDisplay displayObject={this.state.displayObject} backToDisplayTable={this.backToDisplayTable} properties={properties}/>
         }
       </div>
     );
--- src/DataDisplay.js
+++ src/DataDisplay.js
@@ -1,19 +1,42 @@
 import React from 'react'
+import { Table, Container } from 'semantic-ui-react';
 
 class DataDisplay extends React.Component{
     render(){
         let displayObject = this.props.displayObject
+
+        const properties = this.props.properties
+        const headerCell = properties.map( property => (property == 'impressions' || property == 'clicks') ? <Table.HeaderCell rowSpan='2' onClick={() => this.props.sortTableData(property) }>{property} (click to sort)</Table.HeaderCell> : <Table.HeaderCell rowSpan='2'>{property}</Table.HeaderCell>)
+
         return(
             <div>
                 <button onClick={() => this.props.backToDisplayTable()}>Back</button>
-                <h3>Display card</h3>
-                id: {displayObject.id}<br/>
-                pdf: {displayObject.pdf}<br/>
-                text: {displayObject.text}<br/>
-                url: {displayObject.url}<br/>
-                impressions: {displayObject.impressions}<br/>
-                clicks: {displayObject.clicks}<br/>
-                image: {displayObject.image}
+
+                <Container>
+                <h3>Displaying...</h3>
+                <Table celled structured striped >
+                    <Table.Header>
+                        <Table.Row>
+                            {headerCell}
+                        </Table.Row>
+                    </Table.Header>
+                    <Table.Body>
+                        <Table.Row>
+                            <Table.Cell>{displayObject.id}</Table.Cell>
+                            <Table.Cell>{displayObject.pdf}</Table.Cell>
+                            <Table.Cell>{displayObject.text}</Table.Cell>
+                            <Table.Cell>{displayObject.url}</Table.Cell>
+                            <Table.Cell>{displayObject.impressions}</Table.Cell>
+                            <Table.Cell>{displayObject.clicks}</Table.Cell>
+                            <Table.Cell>amount: {displayObject.spend.amount == null ? 'N/A' : displayObject.spend.amount } / currency: {displayObject.spend.currency == null ? 'N/A' : displayObject.spend.currency }</Table.Cell>
+                            <Table.Cell>{displayObject.created == null ? 'N/A' : displayObject.created}</Table.Cell>
+                            <Table.Cell>{displayObject.ended == null ? 'N/A' : displayObject.ended}</Table.Cell>
+                            <Table.Cell>Disclosed</Table.Cell>
+                            <Table.Cell>{displayObject.image}</Table.Cell>
+                        </Table.Row>
+                    </Table.Body>
+                </Table>
+                </Container>
             </div>
         )
     }
